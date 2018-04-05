b2eb7fafb776c7e2fc253ae5069c33d63733c8d9
adriengallou <adrien.gallou95@orange.fr>
2018-04-05 11:07:27 +0200

    cleanage code

--- soustraire_mot-de-liaison.py
+++ soustraire_mot-de-liaison.py
@@ -4,24 +4,40 @@
 import nltk
 from nltk.tokenize import word_tokenize
 
-file = open('./article-essor.txt', 'rt')
-article = file.read()
+#Ouvre les sources
+file0 = open('./article-essor.txt', 'rt')
+source = file0.read()
 
-article = word_tokenize(article, language='french')
+file1 = open('./dico.txt', 'rt')
+dico = file1.read()
 
-new_word = []
+#Coupe la source en mot et le stocke dans un tableau => article
+def tokenize_text(text):
+    return word_tokenize(text, language='french')
 
-for word in article:
-    if len(word) > 3:
-    	# remove capital letters (string operation)
-    	word = word.lower()
-    	# Clean punctuation (string operation)
-    	word = word.strip(" ;''?:,()!.\").”-«»’")
-    	# add word to new word list
-    	new_word.append(word)
+#Trie dans le tableau article, supprime la ponctuation, supprime les mots inférieur à trois lettres
+def clean_text(text):
 
-print(new_word)
+    new_word = []
+
+    for word in text:
+        if len(word) > 3:
+        	# remove capital letters (string operation)
+        	word = word.lower()
+        	# Clean punctuation (string operation)
+        	word = word.strip(" ;''?:,()!.\").”-«»’1234567890")
+        	# add word to new word list
+        	new_word.append(word)
+
+    return new_word
+
+#Run
+article = tokenize_text(source)
+out = clean_text(article)
+
+#Affiche le résultat
+print(out)
 
 
-file.close()
-#destination.close()
+file0.close()
+file1.close()
